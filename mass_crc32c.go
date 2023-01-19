4cac2e7c34125d1cba1fa0b41fd49b4e8f84783e
Stan Drapkin <sdrapkin@sdprime.com>
2023-01-19 16:03:36 -0500

    Add file size

--- mass_crc32c.go
+++ mass_crc32c.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/base64"
 	"encoding/binary"
+	"errors"
 	"flag"
 	"fmt"
 	"hash/crc32"
@@ -13,17 +14,23 @@ import (
 	"runtime"
 	"sync"
 	"time"
-)
+) //import
 
 var wg sync.WaitGroup
-var pathQueue chan string
+
+type job struct {
+	path string
+	size int64
+} //job struct
+
+var jobQueue chan job
 var global_crc32cTable *crc32.Table
 
 func printErr(path string, err error) {
 	fmt.Fprintf(os.Stderr, "error: '%s': %v\n", path, err)
-}
+} //printErr()
 
-func CRCReader(path string, buffer *[]byte) (string, error) {
+func CRCReader(path string, fileInfoSize int64, buffer *[]byte) (string, error) {
 	file, err := os.Open(path)
 	if err != nil {
 		printErr(path, err)
@@ -32,37 +39,43 @@ func CRCReader(path string, buffer *[]byte) (string, error) {
 	defer file.Close()
 
 	checksum := uint32(0)
+	processedSize := int64(0)
 	for {
 		switch n, err := file.Read(*buffer); err {
 		case nil:
 			checksum = crc32.Update(checksum, global_crc32cTable, (*buffer)[:n])
+			processedSize += int64(n)
 		case io.EOF:
 			binary.BigEndian.PutUint32(*buffer, checksum)
 			str := base64.StdEncoding.EncodeToString((*buffer)[:4])
+
+			if fileInfoSize != processedSize {
+				return "", errors.New("fileInfoSize != processedSize")
+			}
 			return str, nil
 		default:
 			return "", err
 		}
 	}
-}
+} //CRCReader()
 
 func fileHandler(bufferSizeKB int) error {
 	wg.Add(1)
 	defer wg.Done()
 	buffer := make([]byte, 1024*bufferSizeKB)
-	for path := range pathQueue { // consume the messages in the queue
+	for work := range jobQueue { // consume the messages in the queue
 
-		crc, err := CRCReader(path, &buffer)
+		crc, err := CRCReader(work.path, work.size, &buffer)
 		if err != nil {
-			printErr(path, err)
+			printErr(work.path, err)
 			continue
 		}
-		fmt.Printf("%s %s\n", crc, path)
+		fmt.Printf("%s %016x %s\n", crc, work.size, work.path)
 	}
 	return nil
-}
+} //fileHandler()
 
-func walkHandler(path string, info os.FileInfo, err error) error {
+func enqueueJob(path string, info os.FileInfo, err error) error {
 	if err != nil {
 		nodeType := "file"
 		if info.IsDir() {
@@ -79,9 +92,9 @@ func walkHandler(path string, info os.FileInfo, err error) error {
 		fmt.Fprintf(os.Stderr, "ignoring: %s\n", path)
 		return nil
 	}
-	pathQueue <- path // add a path message to the queue (blocking when queue is full)
+	jobQueue <- job{path: path, size: info.Size()} // add new file job to the queue (blocking when queue is full)
 	return nil
-}
+} //enqueueJob()
 
 func sanityCheck() {
 	const data = "861844d6704e8573fec34d967e20bcfef3d424cf48be04e6dc08f2bd58c729743371015ead891cc3cf1c9d34b49264b510751b1ff9e537937bc46b5d6ff4ecc8" // sha512("Hello World!")
@@ -95,12 +108,12 @@ func sanityCheck() {
 		fmt.Fprintf(os.Stderr, "Sanity Check failed! Terminating.\n")
 		os.Exit(2)
 	}
-}
+} //sanityCheck()
 
 func printUsage() {
 	fmt.Fprintf(os.Stderr, "Usage of %s: [options] path [path ...]\n\nOptions:\n", os.Args[0])
 	flag.PrintDefaults()
-}
+} //printUsage()
 
 func main() {
 	p := flag.Int("p", runtime.NumCPU(), "# of cpu used")
@@ -120,8 +133,8 @@ func main() {
 	global_crc32cTable = crc32.MakeTable(crc32.Castagnoli)
 	sanityCheck()
 
-	runtime.GOMAXPROCS(*p)            // limit number of kernel threads (CPUs used)
-	pathQueue = make(chan string, *l) // use a channel with a size to limit the number of list ahead path
+	runtime.GOMAXPROCS(*p)        // limit number of kernel threads (CPUs used)
+	jobQueue = make(chan job, *l) // use a channel with a size to limit the number of list ahead path
 
 	start := time.Now()
 
@@ -130,13 +143,13 @@ func main() {
 		go fileHandler(*s)
 	}
 	for _, arg := range flag.Args() {
-		err := filepath.Walk(arg, walkHandler)
+		err := filepath.Walk(arg, enqueueJob)
 		if err != nil {
 			log.Fatal(err)
 		}
 	}
-	close(pathQueue)
+	close(jobQueue)
 	wg.Wait()
 
 	fmt.Fprintln(os.Stderr, time.Since(start))
-}
+} //main()
